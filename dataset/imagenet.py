945a1905d9b70d2107f4a4dee4c736ff465cacf7
Federico Baldassarre <baldassarre.fe@gmail.com>
2017-04-26 15:41:30 +0200

    Integration of resize step in the pipeline

--- dataset/imagenet.py
+++ dataset/imagenet.py
@@ -1,15 +1,13 @@
 import argparse
 import hashlib
-import os
 import sys
 import tarfile
 import urllib.request
 from itertools import islice
-from PIL import Image
-#TO INSTALL: pip install python-resize-image
-from resizeimage import resizeimage
-
+from os.path import join
 from typing import Union, List
+
+from .shared import *
 
 
 # TODO We want this batch to be a tensorflow-compatible batch
@@ -25,15 +23,6 @@ class Batch:
 links_source_url = 'http://image-net.org/imagenet_data/urls/imagenet_fall11_urls.tgz'
 compressed_links_file = 'imagenet_fall11_urls.tgz'
 links_file = 'fall11_urls.txt'
-dir_root = 'imagenet'
-dir_originals = 'original/'
-dir_resized = 'resized/'
-dir_filtered = 'filtered/'
-
-
-def check_folders_exist(folder):
-    if not os.path.isdir(folder):
-        os.mkdir(folder)
 
 
 # NOTE:
@@ -48,14 +37,12 @@ def check_links_file_exists():
             tar.extractall()
 
 
-def image_name(image_url: str, suffix='') -> str:
-    return '{}{}.jpeg'.format(
-        hashlib.md5(image_url.encode('utf-8')).hexdigest(),
-        suffix)
+def image_name(image_url: str) -> str:
+    return hashlib.md5(image_url.encode('utf-8')).hexdigest() + '.jpg'
 
 
 def get_image(image_url: str) -> Union[str, None]:
-    image_path = dir_root + '/' + dir_originals + image_name(image_url)
+    image_path = join(dir_originals, image_name(image_url))
     if not os.path.isfile(image_path):
         try:
             urllib.request.urlretrieve(image_url, image_path)
@@ -73,7 +60,10 @@ def image_urls_generator():
                 line = sources.readline()
                 if line == '':
                     return
-                yield line.split()[1]
+                split = line.split()
+                if len(split) != 2:
+                    continue
+                yield split[1]
             except UnicodeDecodeError as ue:
                 print('Unicode error: {}'.format(ue), file=sys.stderr)
 
@@ -83,42 +73,25 @@ def get_images(size=10, skip=0) -> List[str]:
     valid_images = filter(lambda x: x is not None, (get_image(url) for url in urls))
     return list(valid_images)
 
-def Resize_Img ( filename , save_path , size = [299, 299] ):
-	img = Image.open( filename )
-	# Resize using padding
-	cover = resizeimage.resize_contain(img, size)
-	# Make dir if it does not exist
-	try:
-		os.stat(save_path)
-	except:
-		os.mkdir(save_path)  	
-	# Safe resized image
-	cover.save( save_path + filename + '-resized.jpeg', img.format)
-
 
 if __name__ == '__main__':
     # Argparse setup
     parser = argparse.ArgumentParser(description='Download and process images from imagenet')
     parser.add_argument('-c', '--count', default=10, type=int,
                         help='get COUNT images (default: 10)')
-    parser.add_argument('-f', '--source-file', default=None, type=str, metavar='FILE', dest='source',
-                        help='set the path for the links source file')
-    parser.add_argument('-o', '--image-folder', default=None, type=str, metavar='FOLDER', dest='root',
-                        help='use FOLDER as root of the images folders (default: imagenet)')
+    parser.add_argument('-f', '--source-file', default=links_source_url, type=str,
+                        help='set the path for the links source file (default: {})'
+                        .format(links_source_url), metavar='FILE', dest='source', )
+    parser.add_argument('-o', '--output-folder', default=dir_originals, type=str,
+                        help='use FOLDER to store the images (default: {})'
+                        .format(dir_originals), metavar='FOLDER', dest='output', )
 
-    # Using the arguments
     args = parser.parse_args()
-    if args.source is not None:
-        links_file = args.source
-    if args.root is not None:
-        dir_root = args.root
+    dir_originals = args.output
 
     # Set up folders
-    check_folders_exist(dir_root)
-    check_folders_exist(dir_root + '/' + dir_originals)
-    check_folders_exist(dir_root + '/' + dir_resized)
-    check_folders_exist(dir_root + '/' + dir_filtered)
+    maybe_create_folder(dir_originals)
 
+    # Download images
     for img_path in get_images(args.count):
         print(img_path)
-		Resize_Img(img_path, './Resized/')
