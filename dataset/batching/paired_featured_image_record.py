90b1631766eee334ae594a50efe7ba0b73944e51
Federico Baldassarre <baldassarre.fe@gmail.com>
2017-05-05 18:38:38 +0200

    Reader for the image pair + features TFRecord

--- dataset/batching/paired_featured_image_record.py
+++ dataset/batching/paired_featured_image_record.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-from dataset.batching.tf_record_base import RecordWriter
+from dataset.batching.tf_record_base import RecordWriter, RecordReader
 
 
 class ImagePairRecordWriter(RecordWriter):
@@ -24,3 +24,35 @@ class ImagePairRecordWriter(RecordWriter):
             'target_image': self._bytes_feature(target_image.tostring())
         }))
         self._writer.write(example.SerializeToString())
+
+
+class ImageRecordReader(RecordReader):
+    def _create_read_operation(self):
+        features = tf.parse_single_example(
+            self._tfrecord_serialized,
+            features={
+                'input_file': tf.FixedLenFeature([], tf.string),
+                'input_image': tf.FixedLenFeature([], tf.string),
+                'input_embedding': tf.FixedLenFeature([], tf.string),
+                'target_file': tf.FixedLenFeature([], tf.string),
+                'target_image': tf.FixedLenFeature([], tf.string),
+            })
+        shape = [299, 299, 3]
+
+        # Filenames
+        input_file = features['input_file']
+        target_file = features['target_file']
+
+        # Images
+        input_image = tf.decode_raw(features['input_image'], tf.uint8)
+        input_image = tf.reshape(input_image, shape)
+        input_image = tf.cast(input_image, tf.float32) * (1. / 255)
+        target_image = tf.decode_raw(features['target_image'], tf.uint8)
+        target_image = tf.reshape(target_image, shape)
+        target_image = tf.cast(target_image, tf.float32) * (1. / 255)
+
+        # Embeddings
+        input_embedding = tf.decode_raw(features['target_image'], tf.float32)
+
+        return [input_file, input_image, input_embedding, target_file,
+                target_image]
